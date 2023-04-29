865db00331661d3379d78c15ce59b26b745b0b36
fscarmen2 <fs.carmen@gmail.com>
2023-04-29 19:59:36 +0800

    Update keeping alive.

--- files/server.js
+++ files/server.js
@@ -1,5 +1,6 @@
 const username = process.env.WEB_USERNAME || "admin";
 const password = process.env.WEB_PASSWORD || "password";
+const url = `https://${process.env.CSB_SANDBOX_ID}-3000.${process.env.CSB_BASE_PREVIEW_HOST}`
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
@@ -98,7 +99,7 @@ app.get("/test", function (req, res) {
 //web保活
 function keep_web_alive() {
   // 请求主页，保持唤醒
-  exec("curl -m8 127.0.0.1:" + port, function (err, stdout, stderr) {
+  exec("curl -m8 " + url, function (err, stdout, stderr) {
     if (err) {
       console.log("保活-请求主页-命令行执行错误：" + err);
     }
